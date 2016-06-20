24433dce72289d8a51bc921d1d57c73f39ea87cb
anna <1085177899@qq.com>
2016-06-20 21:55:49 +0800

    update filter for product list

--- default/themes/cms_theme/template.php
+++ default/themes/cms_theme/template.php
@@ -5,7 +5,10 @@
 function cms_theme_form_views_exposed_form_alter(&$form, &$form_state) {
   if($form['#id'] == 'views-exposed-form-products-page-1' ||
     $form['#id'] == 'views-exposed-form-products-page-2') {
-    $form['field_color_value']['#options']['All'] = '任意';
+    $form['field_color_value']['#options']['All'] = t('所有颜色');
+    $form['field_chuang_tou_gui_value']['#options']['All'] = t('所有型号');
+    $form['field_code_value']['#attributes']['placeholder'] = t('例如: 218#');
+    $form['field_code_value']['#attributes']['size'] = 10;
     $form['submit']['#value'] = '查找';
   }
 }
